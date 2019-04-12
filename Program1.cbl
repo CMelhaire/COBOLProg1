ff758282dec0013cf63995efe927e77f467cab37
Jack Rasmussen <rasmussenjac@uwplatt.edu>
2019-04-12 10:39:08 -0500

    ADDED RUNNING TOTALS, CALCULATE FUNCTIONS FOR RUNNING TOTALS AND MORE FORMATTING

--- Program1.cbl
+++ Program1.cbl
@@ -30,11 +30,14 @@
 			   05 FILLER  PIC x VALUE SPACES.
                05 CUSTO-NAME PIC A(20).
 			   05 FILLER  PIC x VALUE SPACES.
-               05 UNIT-PRICES PIC 9(5).
+               05 UNIT-PRICES PIC 9(5) VALUE 0.
 			   05 FILLER  PIC x VALUE SPACES.
                05 QUANTITYS-SOLD PIC 9(3).
            01 TEMP-VAR.
 			   05 NEWCUST PIC A.
+			   05 TEMP-SALE PIC 9(5).
+			   05 TEMP-TAX PIC 9(5).
+			   05 TEMP-FINAL PIC 9(5).
 		   01 COL-HDR.
                05  FILLER      PIC X(4)   VALUE "#".
                05  FILLER      PIC X      VALUE SPACES.
@@ -43,10 +46,17 @@
                05  FILLER      PIC X(5)   VALUE "PRICE".
 			   05  FILLER      PIC X      VALUE SPACES.
                05  FILLER      PIC X(9)   VALUE "QTY".
-           01 EOF-REPORT.
-			   05 
-
-
+           01 REPORT-TOTAL.
+			   05 FILLER       PIC X(14)  VALUE "Total Sales: $".
+			   05 TOTAL-SALE   PIC 9(5) VALUE 0.
+		   01 REPORT-TAX.
+			   05 FILLER       PIC X(14)  VALUE "Sales Tax:   $".
+			   05 SALES-TAX    PIC 9(5) VALUE 0.
+		   01 REPORT-FINAL.
+               05 FILLER       PIC x(14)  VALUE "Final Sales: $".
+			   05 FINAL-SALE   PIC 9(5) VALUE 0.
+           01 WS-CONSTANTS.
+			   05 WS-TAX       PIC 9v99 VALUE 0.05.
                
 
 
@@ -56,6 +66,7 @@
 		   WRITE SALES-FILE-ID FROM COL-HDR.
 		   DISPLAY"ADD NEW CUSTOMER - Y OR N" ACCEPT NEWCUST
                PERFORM 100-LOOP UNTIL NEWCUST EQUALS "N" 
+			   PERFORM 100-WRITE-REPORT
 		   CLOSE SALES-FILE.
 
 
@@ -72,7 +83,26 @@
 
                    MOVE SALESFILES-ID to SALES-FILE-ID
                    WRITE SALES-FILE-ID.
+				   PERFORM 100-CALCULATE-TOTALS
 				   DISPLAY"ADD NEW CUSTOMER - Y OR N"
-                   ACCEPT NEWCUST
+                   ACCEPT NEWCUST.
 
+       100-WRITE-REPORT.
+		   WRITE SALES-FILE-ID FROM REPORT-TOTAL
+		   WRITE SALES-FILE-ID FROM REPORT-TAX
+		   WRITE SALES-FILE-ID FROM REPORT-FINAL.
+
+	   100-CALCULATE-TOTALS.
+		   IF QUANTITYS-SOLD > 0
+		   COMPUTE TEMP-SALE = QUANTITYS-SOLD * UNIT-PRICES
+			   ADD TEMP-SALE to TOTAL-SALE
+			   COMPUTE TEMP-TAX = (WS-TAX * TEMP-SALE)
+			   ADD TEMP-TAX TO SALES-TAX
+		   ELSE IF QUANTITYS-SOLD < 0
+		   COMPUTE TEMP-SALE = QUANTITYS-SOLD * UNIT-PRICES
+			   subtract TEMP-SALE from TOTAL-SALE
+			   COMPUTE TEMP-TAX = (WS-TAX * TEMP-SALE)
+			   subtract TEMP-TAX from SALES-TAX
+           END-IF
+		   COMPUTE FINAL-SALE = SALES-TAX + TOTAL-SALE.
        end program Program1.
