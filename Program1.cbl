70ff16500b36b04dce19b47934b908d3fd69c493
CMelhaire <45501312+CMelhaire@users.noreply.github.com>
2019-04-13 16:07:36 -0500

    Update 4-13-2019
    Use this one

--- Program1.cbl
+++ Program1.cbl
@@ -1,63 +1,79 @@
-       identification division.
-       program-id. Program1.
-
-       environment division.
-           INPUT-OUTPUT SECTION.
-               FILE-CONTROL.
-                   SELECT SALES-FILE ASSIGN TO
-       "C:\Users\thoma\source\repos\HaylockGrant\COBOLProg1\test.txt"
-                       ORGANIZATION IS LINE SEQUENTIAL
-                       ACCESS IS SEQUENTIAL.
-
-
-       configuration section.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGRAM1.
+      ******************************************************************
+      *    Creates a sales transaction file to record a series of 
+      *    transactions.  User determines the number of records created
+      *    and enters all information through the console.  Also
+      *    creates a report file to tell if a record was entered into
+      *    the sales file or not.
+      *
+      *    Authors: Grant Haylock, Jack Rasmussen,
+      *             Zach Schultz, Austin Lee
+      *    Date Written: 04-07-2019
+      ******************************************************************
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT SALES-FILE ASSIGN TO
+       "C:\Users\Caulder\Documents\test.txt"
+           ORGANIZATION IS LINE SEQUENTIAL
+           ACCESS IS SEQUENTIAL.
+       CONFIGURATION SECTION.
            
-
-       data division.
+       DATA DIVISION.
        FILE SECTION.
-           FD SALES-FILE.
-           01 SALES-FILE-ID.
-               05 CUST-NO PIC 9(4).
-			   05 FILLER  PIC x VALUE SPACES.
-               05 CUST-NAME PIC A(20).
-			   05 FILLER  PIC x VALUE SPACES.
-               05 UNIT-PRICE PIC 9(5).
-			   05 FILLER  PIC x VALUE SPACES.
-               05 QUANTITY-SOLD PIC 9(3).
-       working-storage section.
-           01 SALESFILES-ID.
-               05 CUST-NUM PIC 9(4).
-			   05 FILLER  PIC x VALUE SPACES.
-               05 CUSTO-NAME PIC A(20).
-			   05 FILLER  PIC x VALUE SPACES.
-               05 UNIT-PRICES PIC 9(5) VALUE 0.
-			   05 FILLER  PIC x VALUE SPACES.
-               05 QUANTITYS-SOLD PIC 9(3).
-           01 TEMP-VAR.
-			   05 NEWCUST PIC A.
-			   05 TEMP-SALE PIC 9(5).
-			   05 TEMP-TAX PIC 9(5).
-			   05 TEMP-FINAL PIC 9(5).
-		   01 COL-HDR.
-               05  FILLER      PIC X(4)   VALUE "#".
-               05  FILLER      PIC X      VALUE SPACES.
-               05  FILLER      PIC X(20)  VALUE "Name". 
-			   05  FILLER      PIC X      VALUE SPACES.
-               05  FILLER      PIC X(5)   VALUE "PRICE".
-			   05  FILLER      PIC X      VALUE SPACES.
-               05  FILLER      PIC X(9)   VALUE "QTY".
-           01 REPORT-TOTAL.
-			   05 FILLER       PIC X(14)  VALUE "Total Sales: $".
-			   05 TOTAL-SALE   PIC 9(5) VALUE 0.
-		   01 REPORT-TAX.
-			   05 FILLER       PIC X(14)  VALUE "Sales Tax:   $".
-			   05 SALES-TAX    PIC 9(5) VALUE 0.
-		   01 REPORT-FINAL.
-               05 FILLER       PIC x(14)  VALUE "Final Sales: $".
-			   05 FINAL-SALE   PIC 9(5) VALUE 0.
-           01 WS-CONSTANTS.
-			   05 WS-TAX       PIC 9v99 VALUE 0.05.
-             01  MICROFOCUS-COLORS  PIC 99.
+       FD SALES-FILE.
+       01 SALES-FILE-ID.
+           05 CUST-NO PIC 9(4).
+		   05 FILLER  PIC x VALUE SPACES.
+           05 CUST-NAME PIC A(20).
+		   05 FILLER  PIC x VALUE SPACES.
+           05 UNIT-PRICE PIC 9(5).
+		   05 FILLER  PIC x VALUE SPACES.
+           05 QUANTITY-SOLD PIC 9(3).
+
+       WORKING-STORAGE SECTION.
+       01 SALESFILES-ID.
+           05 CUST-NUM PIC 9(4).
+		   05 FILLER  PIC x VALUE SPACES.
+           05 CUSTO-NAME PIC A(20).
+		   05 FILLER  PIC x VALUE SPACES.
+           05 UNIT-PRICES PIC 9(5) VALUE 0.
+		   05 FILLER  PIC x VALUE SPACES.
+           05 QUANTITYS-SOLD PIC 9(3).
+       01 TEMP-VAR.
+		   05 NEWCUST PIC A.
+		   05 TEMP-SALE PIC 9(5).
+		   05 TEMP-TAX PIC 9(5).
+		   05 TEMP-FINAL PIC 9(5).
+	   01 COL-HDR.
+           05  FILLER      PIC X(4)   VALUE "#".
+           05  FILLER      PIC X      VALUE SPACES.
+           05  FILLER      PIC X(20)  VALUE "Name". 
+		   05  FILLER      PIC X      VALUE SPACES.
+           05  FILLER      PIC X(5)   VALUE "PRICE".
+		   05  FILLER      PIC X      VALUE SPACES.
+           05  FILLER      PIC X(9)   VALUE "QTY".
+       01 REPORT-TOTAL.
+		   05 FILLER           PIC X(14)
+               VALUE "Total Sales: ".
+		   05 TOTAL-SALE-OUT   PIC $$,$$$           VALUE 0.
+	   01 REPORT-TAX.
+		   05 FILLER           PIC X(14)
+               VALUE  "Sales Tax:   ".
+		   05 SALES-TAX-OUT    PIC $$,$$$           VALUE 0.
+	   01 REPORT-FINAL.
+           05 FILLER           PIC x(14)
+               VALUE "Final Sales: ".
+		   05 FINAL-SALE-OUT   PIC $$,$$$           VALUE 0.
+       01 WS-CONSTANTS.
+		   05 WS-TAX           PIC 9v999 VALUE 0.065.
+       01  WS-WORK-AREA.
+           05  TOTAL-SALE      PIC 9(5).
+           05  SALES-TAX       PIC 9(5).
+           05  FINAL-SALE      PIC 9(5).
+
+       01  MICROFOCUS-COLORS   PIC 99.
       *THESE COLORS CNA BE USED FOR FOREGROUND AND BACKGROUND.
            78  BLACK                            VALUE 0.
            78  BLUE                             VALUE 1.
@@ -76,56 +92,78 @@
            78  BRIGHT-MAGENTA                   VALUE 13.
            78  BRIGHT-BROWN                     VALUE 14.
            78  BRIGHT-WHITE                     VALUE 15.  
-
-
-       procedure division.
+       PROCEDURE DIVISION.
+      ******************************************************************
+      *    100-MAIN-MODULE: Opens Ssales file and report file.   
+      *    Writes headers to the sales file.  Prompts user if they  
+      *    want to add a new customer record, and then calls a module
+      *    to input the customer information.
+      *    Calls a module to write the data to the files. 
+      *    Lastly closes all files and ends the program.
+      ******************************************************************
        100-MAIN.
            OPEN EXTEND SALES-FILE.
 		   WRITE SALES-FILE-ID FROM COL-HDR.
            DISPLAY"ADD NEW CUSTOMER - Y OR N" ACCEPT NEWCUST  
-				   
-           PERFORM 100-LOOP UNTIL NEWCUST EQUALS "N"
-			   PERFORM 100-WRITE-REPORT
+           PERFORM 200-GET-CUST-INFO UNTIL NEWCUST EQUALS "N"
+			   PERFORM 400-WRITE-REPORT
 		   CLOSE SALES-FILE.
-
-
-           goback.
-       100-LOOP.
-				   
-		           DISPLAY "CUSTOMER-NO: "
+           GOBACK
+           .
+      ******************************************************************
+      *    200-GET-CUST-INFO: Displays prompts for customer information
+      *    and then accepts the input.  Calls a module to calculate the
+      *    totals and taxes, and then prompts the user to input
+      *    another record if they would like to.
+      ******************************************************************
+       200-GET-CUST-INFO.
+		           DISPLAY "CUSTOMER NO: "
                    ACCEPT CUST-NUM
-                   DISPLAY "CUSTOMER-NAME: "
+                   DISPLAY "CUSTOMER NAME: "
                    ACCEPT CUSTO-NAME
-                   DISPLAY "UNIT-PRICE INPUT: "
+                   DISPLAY "UNIT PRICE INPUT: "
                    ACCEPT UNIT-PRICES
-                   DISPLAY "QUNATITYS-SOLD: "
+                   DISPLAY "QUANTITIES SOLD: "
                    ACCEPT QUANTITYS-SOLD
-
                    MOVE SALESFILES-ID to SALES-FILE-ID
                    WRITE SALES-FILE-ID.
-				   PERFORM 100-CALCULATE-TOTALS
+				   PERFORM 300-CALCULATE-TOTALS
 				   DISPLAY"RECORD ADDED. ADD ANOTHER CUSTOMER - Y OR N"
-                   ACCEPT NEWCUST.
-
-       100-WRITE-REPORT.
-		   COMPUTE FINAL-SALE = TOTAL-SALE + SALES-TAX
-		   WRITE SALES-FILE-ID FROM REPORT-TOTAL
-		   WRITE SALES-FILE-ID FROM REPORT-TAX
-		   WRITE SALES-FILE-ID FROM REPORT-FINAL.
-
-	   100-CALCULATE-TOTALS.
+                   ACCEPT NEWCUST
+                   .
+      ******************************************************************
+      *    300-CALCULATE-TOTALS: Checks to see if quantity entered
+      *    above is positive/negative.  If negative, it subtracts 
+      *    quantity * price from the running sales total.  If positive, 
+      *    it adds quantity * price to the running total.
+      *    Calculates sales tax and a subtotal.
+      ******************************************************************
+	   300-CALCULATE-TOTALS.
 		   IF QUANTITYS-SOLD > 0
 		   COMPUTE TEMP-SALE = QUANTITYS-SOLD * UNIT-PRICES
-			   ADD TEMP-SALE to TOTAL-SALE
+			   ADD TEMP-SALE TO TOTAL-SALE
 			   COMPUTE TEMP-TAX = (WS-TAX * TEMP-SALE)
 			   ADD TEMP-TAX TO SALES-TAX
 		   ELSE IF QUANTITYS-SOLD < 0
 		   COMPUTE TEMP-SALE = QUANTITYS-SOLD * UNIT-PRICES
-			   subtract TEMP-SALE from TOTAL-SALE
+			   SUBTRACT TEMP-SALE FROM TOTAL-SALE
 			   COMPUTE TEMP-TAX = (WS-TAX * TEMP-SALE)
-			   subtract TEMP-TAX from SALES-TAX
-			   
-			  
+			   SUBTRACT TEMP-TAX FROM SALES-TAX
            END-IF
-		   
-       end program Program1.
+           .
+      ******************************************************************
+      *    400-WRITE-REPORT: Computes the final total sales value,
+      *    and then writes to the sales file.
+      ******************************************************************
+       400-WRITE-REPORT.
+		   COMPUTE FINAL-SALE = TOTAL-SALE + SALES-TAX
+           MOVE TOTAL-SALE TO TOTAL-SALE-OUT
+           MOVE SALES-TAX TO SALES-TAX-OUT
+           MOVE FINAL-SALE TO FINAL-SALE-OUT
+
+		   WRITE SALES-FILE-ID FROM REPORT-TOTAL
+		   WRITE SALES-FILE-ID FROM REPORT-TAX
+		   WRITE SALES-FILE-ID FROM REPORT-FINAL.
+
+			 
+       END PROGRAM PROGRAM1.
